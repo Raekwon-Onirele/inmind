0a53b9c186ff6951a7d17017477b9e5b3c7c8752
Raekwon-Onirele <contato.raekwon@gmail.com>
2025-11-17 11:14:13 -0300

    Alteração menu part1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import imgInMind from "../assets/InMind_page-0001.png";
 import ButtonEnter from "../components/ButtonEnter";
 import styles from "./Home.css";
@@ -6,22 +7,47 @@ const iconHome =
   "https://img.icons8.com/?size=100&id=vSCcbxMrzsbl&format=png&color=000000";
 
 const Home = () => {
+  {
+    /* Função para do Menu Início e Quem Somos */
+  }
+  const [active, setMode] = useState(false);
+  const ToggleMode = () => {
+    setMode(!active);
+  };
+
   return (
     <div>
       {/* Nav-Bar da página */}
       <nav className="nav-bar">
+        {/* Logo da marca no Nav-bar */}
         <img src={imgInMind} alt="Foto da logo" className="imgInMind" />
+
+        {/* Barra menu Início e Quem Somos */}
         <div className="menu">
-          <a href="a" className="textMenu">
-            Início
-          </a>
-          <img src={iconHome} alt="logo saduiche" className="iconHome" />
+          <div className="menuButton" onClick={ToggleMode}>
+            {active ? "Fechar X" : "Menu"}
+            <a href="a" className="textMenu">
+              Início
+            </a>
+            <img src={iconHome} alt="logo saduiche" className="iconHome" />
+          </div>
+          <div className={"menu-dropdown ${active"}>aaaa
+            <ul>
+              <li>
+                <a href="a">Agende Sua Consulta</a>
+              </li>
+              <li>
+                <a href="a">Nosso Whatsapp</a>
+              </li>
+            </ul>
+          </div>
+
           <a href="" className="textMenu">
             Quem Somos
           </a>
           <img src={iconHome} alt="logo saduiche" className="iconHome" />
         </div>
-        <ButtonEnter className="buttonEnter"/>
+        <ButtonEnter className="buttonEnter" />
       </nav>
       {/* Nav-Bar da página */}
 
