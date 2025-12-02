5a944cddcac2691cee437af600a912f9461cae0f
Raekwon-Onirele <contato.raekwon@gmail.com>
2025-12-02 16:25:36 -0300

    Criação do form

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -2,23 +2,34 @@ import "./FormPage.css";
 import imgMenuVoltar from "../assets/menu_voltar.png";
 
 const FormPage = ({ setOpenFormPage }) => {
+
+
   return (
     <div className="backPage">
       <div className="formPage">
-          <img
-            src={imgMenuVoltar}
-            alt="foto_menu_voltar"
-            onClick={setOpenFormPage}
-          />
+        <img
+          src={imgMenuVoltar}
+          alt="foto_menu_voltar"
+          onClick={setOpenFormPage}
+        />
 
-          <h1 className="titleForm">Preencha o Formulário: </h1>
+        <h1 className="titleForm">Preencha o Formulário: </h1>
 
-          <form className="form">
-            <label htmlFor="">
-              <span>Nome: </span>
-              <input type="text" placeholder="Nome Completo" />
-            </label>
-          </form>
+        <form className="form">
+          <label htmlFor=""> Nome Completo: </label>
+          <input type="text" placeholder="Digite seu nome completo" required minLength={4}/>
+
+          <label htmlFor=""> Email: </label>
+          <input type="email" placeholder="Digite seu email" required />
+
+          <label htmlFor=""> Telefone para Contato: </label>
+          <input type="text" placeholder="(00) 00000-0000" maxLength="15" required />
+
+          <label htmlFor=""> Data de Nascimento: </label>
+          <input type="date" placeholder="Nome Completo" required />
+
+          <button>Continuar</button>
+        </form>
       </div>
     </div>
   );
